859b564fd7f8c0e2e0e846bb10b3bc572f89117d
Guilherme Steinmuller <guilhermesp@sarapo.lsd.ufcg.edu.br>
2015-06-10 18:12:47 -0300

    refactoring

--- aggregate_usage.sh
+++ aggregate_usage.sh
@@ -2,8 +2,13 @@
 
 
 
-
+echo -e "Statistics of aggregate $1\n"
 for i in `nova aggregate-details $1 | grep -v ^+ | grep -v ^"| Id |" | awk -F\| '{ print $5 }'`; do 
-		nova host-describe $(echo $i | sed "s/'//g" | sed "s/,//g"); 
+		echo "Host: $(echo $i | sed "s/'//g" | sed "s/,//g")"
+		total_cpu=$(nova host-describe $(echo $i | sed "s/'//g" | sed "s/,//g") | grep "(total)" | awk '{ print $6 }');
+		cpu_used=$(nova host-describe $(echo $i | sed "s/'//g" | sed "s/,//g") | grep "(used_now)" | awk '{ print $6 }'); 
+	 	max_cpu_usage=$(nova host-describe $(echo $i | sed "s/'//g" | sed "s/,//g") | grep "(used_max)" | awk '{ print $6 }');
+		echo "Total cpu: $total_cpu    Used cpu: $cpu_used      Cpu maximum usage: $max_cpu_usage";
+		echo "" 
 done
 
