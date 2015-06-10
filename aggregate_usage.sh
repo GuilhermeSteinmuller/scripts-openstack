80b0e3d686d306355668e405f7a5dc7950c3f833
Guilherme Steinmuller <guilhermesp@sarapo.lsd.ufcg.edu.br>
2015-06-10 17:22:59 -0300

    showing each host usege that belongs to a  particular aggregate

--- aggregate_usage.sh
+++ aggregate_usage.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 
+
+
+for i in `nova aggregate-details $1 | grep -v ^+ | grep -v ^"| Id |" | awk -F\| '{ print $5 }'`; do 
+		nova host-describe $(echo $i | sed "s/'//g" | sed "s/,//g"); 
+done
+
