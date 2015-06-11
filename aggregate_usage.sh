0d6dd4ef6ad175f7e31b07efe4da687e7a8c691f
Guilherme Steinmuller <guilhermesp@sarapo.lsd.ufcg.edu.br>
2015-06-11 15:05:50 -0300

    refactoring

--- aggregate_usage.sh
+++ aggregate_usage.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 
+#command nova aggregate-details is suitable for nova version >= 2.22.0
 
 
-echo -e "Statistics of aggregate $1\n"
-for i in `nova aggregate-details $1 | grep -v ^+ | grep -v ^"| Id |" | awk -F\| '{ print $5 }'`; do 
-		echo "Host: $(echo $i | sed "s/'//g" | sed "s/,//g")"
-		total_cpu=$(nova host-describe $(echo $i | sed "s/'//g" | sed "s/,//g") | grep "(total)" | awk '{ print $6 }');
-		cpu_used=$(nova host-describe $(echo $i | sed "s/'//g" | sed "s/,//g") | grep "(used_now)" | awk '{ print $6 }'); 
-	 	max_cpu_usage=$(nova host-describe $(echo $i | sed "s/'//g" | sed "s/,//g") | grep "(used_max)" | awk '{ print $6 }');
-		echo "Total cpu: $total_cpu    Used cpu: $cpu_used      Cpu maximum usage: $max_cpu_usage";
-		echo "" 
+#Return the details of a specifc aggregate
+aggregate_details () {
+	nova aggregate-details $1 | grep -v ^+ | grep -v ^"| Id |" | awk -F\| '{ print $5 }'
+
+}
+
+
+host_details(){
+	local host=$(echo $i | sed "s/'//g" | sed "s/,//g")
+	local total_cpu=$( nova host-describe $(echo $host | sed "s/'//g" | sed "s/,//g") | grep "(total)" | awk '{ print $6 }')
+	local cpus_used_now=$( nova host-describe $(echo $host | sed "s/'//g" | sed "s/,//g") | grep "(used_now)" | awk '{ print $6 }')
+	echo "Host: $(echo $i | sed "s/'//g" | sed "s/,//g")"
+	echo -e "CPU: $cpus_used_now/$total_cpu\n";
+	
+}
+
+
+for i in `aggregate_details $1`; do
+      host_details $i;
 done
 
