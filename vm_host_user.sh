7dcf75a202e59344baf3a8cc982c23ca51905452
Guilherme Steinmuller <guilhermesp@sarapo.lsd.ufcg.edu.br>
2015-06-12 17:12:05 -0300

    showing user-vm-host

---
+++ vm_host_user.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+get_user(){
+	user_id=$(nova show $1 | grep "user_id" | awk '{print $4}')
+	echo "$(keystone user-list | grep $user_id | awk '{print $4}')"
+}
+
+get_vm_name(){
+	echo "$(nova show $1 | grep ^"| name" | awk '{print $4}')"
+}
+
+get_host(){
+	echo "$(nova show $1 | grep "OS-EXT-SRV-ATTR:hypervisor_hostname" | awk '{print $4}')"
+}
+
+echo "$(get_vm_name $1) | $(get_host $1) | $(get_user $1)"
+
